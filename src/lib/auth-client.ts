7916e140a7783b82d94126394635ac6caf1b4f3a
patryk <paniczgg@gmail.com>
2025-06-28 08:12:55 +0200

    basic use of better-auth

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -4,4 +4,3 @@ export const authClient = createAuthClient({
   // baseURL: process.env.BETTER_AUTH_URL,
 });
 
-// export const { signIn, signUp, useSession } = createAuthClient();
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,17 @@
-import { db } from "../db/index";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
+import { db } from "@/db";
+import * as schema from "@/db/schema";
+
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
-    provider: "pg", // or "mysql", "sqlite"
+    provider: "pg",
+    schema: {
+      ...schema,
+    },
   }),
-  emailAndPassword: { 
-    enabled: true
+  emailAndPassword: {
+    enabled: true,
   },
 });
