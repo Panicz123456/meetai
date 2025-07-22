8f0cbd2303cffa3a408c89a987b490f3133379fe
patryk <paniczgg@gmail.com>
2025-07-22 17:05:28 +0200

    video call

--- src/modules/call/ui/views/call-view.tsx
+++ src/modules/call/ui/views/call-view.tsx
@@ -1,17 +1,30 @@
-import { useTRPC } from "@/trpc/client"
-import { useSuspenseQuery } from "@tanstack/react-query"
+"use client";
 
-interface Props { 
-	meetingId: string
+import { ErrorState } from "@/components/error-state";
+import { useTRPC } from "@/trpc/client";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { CallProvider } from "../components/call-provider";
+
+interface Props {
+  meetingId: string;
 }
 
-export const CallView = ({meetingId}: Props) => { 
-	const trpc = useTRPC()
-	const { data } = useSuspenseQuery(trpc.meeting.getOne.queryOptions({ id: meetingId }))
-	
-	return ( 
-		<div>
-			{JSON.stringify(data, null, 2)}
-		</div>
-	)
-}
+export const CallView = ({ meetingId }: Props) => {
+  const trpc = useTRPC();
+  const { data } = useSuspenseQuery(
+    trpc.meeting.getOne.queryOptions({ id: meetingId })
+  );
+
+  if (data.status === "completed") {
+    return (
+      <div className="flex h-screen items-center justify-center">
+        <ErrorState
+          title="Meeting has ended"
+          description="You can no longer join this meeting"
+        />
+      </div>
+    );
+  }
+
+  return <CallProvider meetingId={meetingId} meetingName={data.name} />;
+};
